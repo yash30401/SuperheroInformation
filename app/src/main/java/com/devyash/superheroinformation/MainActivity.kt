79f89653bcb0a625f6f7ce4e962e68ea2d9b2b39
yash30401 <yashveersingh30401@gmail.com>
2022-08-10 20:33:54 +0530

    1. Complete

--- app/src/main/java/com/devyash/superheroinformation/MainActivity.kt
+++ app/src/main/java/com/devyash/superheroinformation/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.devyash.superheroinformation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ScrollView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.devyash.superheroinformation.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import org.json.JSONArray
 import org.json.JSONObject
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -20,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
@@ -27,10 +33,21 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.search.setOnClickListener {
+
+            val imm = applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.search.windowToken, 0)
+
+
             if(binding.superheroName.text.isEmpty()){
                 Toast.makeText(applicationContext, "Empty Field!", Toast.LENGTH_SHORT).show()
             }else{
-                GlobalScope.async {
+
+
+                binding.pBar.visibility=View.VISIBLE
+
+
+                GlobalScope.launch {
+
                     getPredictions()
                 }
 
@@ -47,12 +64,25 @@ class MainActivity : AppCompatActivity() {
         try {
 
             val result= GlobalScope.async {
-                callAztroAPI("https://superhero-search.p.rapidapi.com/api/?hero=hulk&rapidapi-key=3dff07b0f5msh40d4c6bb5e5815fp1fabfajsn77cdcf2e6982")
+                var superheroname=binding.superheroName.text.toString()
+                callAztroAPI("https://superhero-search.p.rapidapi.com/api/?hero=${superheroname}&rapidapi-key=3dff07b0f5msh40d4c6bb5e5815fp1fabfajsn77cdcf2e6982")
 
             }.await()
 
-            onResponse(result)
+            Log.d("Result.............",result.toString())
+            if(result.toString()=="Hero Not Found"){
 
+                runOnUiThread {
+                    binding.pBar.visibility = View.INVISIBLE
+                    binding.scrollbar.visibility = View.GONE
+                    binding.NAME.visibility = View.INVISIBLE
+                    binding.heroName.text = "Hero Not Found!!"
+                    binding.namelay.visibility = View.VISIBLE
+                }
+            }else {
+
+                onResponse(result)
+            }
         }catch (e: Exception){
             e.printStackTrace()
         }
@@ -103,32 +133,73 @@ class MainActivity : AppCompatActivity() {
 
     private fun onResponse(result: String?) {
 
-        try {
-            // convert the string to JSON object for better reading
-            val resultJson = JSONObject(result)
-            // Initialize prediction text
-            var prediction ="Data \n"
-            var powerStats:JSONObject
-           
-            // Update text with various fields from response
-            prediction += resultJson.getString("id")+"\n"
 
-            powerStats=resultJson.getJSONObject("powerstats")
 
-            prediction+=powerStats["strength"]
-            //Update the prediction to the view
+            try {
 
-            setText(binding.resultText,prediction)
+                val resultJson = JSONObject(result)
 
-        } catch (e: Exception) {
-            e.printStackTrace()
-           binding.resultText.text = "Oops!! something went wrong, please try again"
+
+
+
+                setText(resultJson)
+
+
+
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                binding.NAME.visibility = View.GONE
+                binding.scrollbar.visibility=View.GONE
+                runOnUiThread{binding.heroName.text = "Oops!! something went wrong, please try again"}
+
+            }
+
+
+    }
+
+    private fun setText(value: JSONObject) {
+
+        var powerstats=value.getJSONObject("powerstats")
+        var appearance=value.getJSONObject("appearance")
+        var biography=value.getJSONObject("biography")
+        var work=value.getJSONObject("work")
+        var connections=value.getJSONObject("connections")
+
+        runOnUiThread{
+            binding.scrollbar.visibility=View.VISIBLE
+
+            binding.NAME.visibility=View.VISIBLE
+            binding.namelay.visibility=View.VISIBLE
+            binding.heroName.text=value.getString("name")
+
+            binding.pBar.visibility=View.GONE
+            binding.intelligancevalue.text=powerstats["intelligence"].toString()
+            binding.strengthValue.text=powerstats["strength"].toString()
+            binding.speedvalue.text=powerstats["speed"].toString()
+            binding.durabilityvalue.text=powerstats["durability"].toString()
+            binding.powervalue.text=powerstats["power"].toString()
+            binding.comatvalue.text=powerstats["combat"].toString()
+            binding.gendervalue.text=appearance["gender"].toString()
+            binding.racevalue.text=appearance["race"].toString()
+            binding.heightvalue.text=appearance["height"].toString()
+            binding.weightvalue.text=appearance["weight"].toString()
+            binding.eyecolorvalue.text=appearance["eyeColor"].toString()
+            binding.haircolorvalue.text=appearance["hairColor"].toString()
+            binding.fullnamevalue.text=biography["fullName"].toString()
+            binding.aliasesvalue.text=biography["aliases"].toString()
+            binding.placeofbirthvalue.text=biography["placeOfBirth"].toString()
+            binding.firstappearncevalue.text=biography["firstAppearance"].toString()
+            binding.publishervalue.text=biography["publisher"].toString()
+            binding.occupationvalue.text=work["occupation"].toString()
+            binding.basevalue.text=work["base"].toString()
+            binding.groupaffvalue.text=connections["groupAffiliation"].toString()
+            binding.relativevalue.text=connections["relatives"].toString()
         }
 
     }
 
-    private fun setText(resultView: TextView, value: String) {
-        runOnUiThread{binding.resultText.text=value}
-        Log.d("Text",value)
-    }
+
+
+
 }
