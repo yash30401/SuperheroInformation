40639fc3ccf3736a435e984476e1ae04df14e9c9
yash30401 <yashveersingh30401@gmail.com>
2022-08-19 14:49:07 +0530

    1. Settings Page Working Implemented

--- app/src/main/java/com/devyash/superheroinformation/Settings.kt
+++ app/src/main/java/com/devyash/superheroinformation/Settings.kt
@@ -1,23 +1,78 @@
 package com.devyash.superheroinformation
 
+import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.devyash.superheroinformation.databinding.ActivitySettingsBinding
 
 class Settings : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding=ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
+        binding.policybtn.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://pages.flycricket.io/superhero-informatio/privacy.html")
+                )
+            )
+        }
+
+        binding.rateappbtn.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=com.devyash.superheroinformation")
+                )
+            )
+        }
+
+        binding.conatctbtn.setOnClickListener {
+            val intent=Intent(Intent.ACTION_SENDTO,Uri.fromParts("mailto","yashquery30401@gmail.com",null))
+            startActivity(Intent.createChooser(intent,"Send Mail..."))
+        }
+
+        binding.howtobtn.setOnClickListener {
+            val dialog= Dialog(this)
+            dialog.setContentView(R.layout.howtodialoglayout)
+
+            dialog.show()
+
+
+            val conslayout=dialog.findViewById<LinearLayout>(R.id.conslayout)
+
+            conslayout.setBackgroundColor(ContextCompat.getColor(applicationContext, android.R.color.transparent))
+
+            if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                conslayout.alpha= 0F
+            } else {
+                conslayout.setBackgroundColor(ContextCompat.getColor(applicationContext, android.R.color.transparent))
+            }
+
+
+            val okbtn=dialog.findViewById<Button>(R.id.okbtn)
+            okbtn.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
 
     }
 
